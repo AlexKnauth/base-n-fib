d0c3a90a3bdb49a0998f917f3e1ecc5f9c12a767
AlexKnauth <alexander@knauth.org>
2015-06-14 14:34:51 -0400

    style

--- base-n-fib.rkt
+++ base-n-fib.rkt
@@ -19,12 +19,14 @@ define find-digits(digitss #:base base)
       error('idontknowwhattodo)
     cons(digits rst)
       my-cond
-        if {base <= bytes-length(digits)} digits
-        else find-digits(rst #:base base)
+        if {base <= bytes-length(digits)}
+          digits
+        else
+          find-digits(rst #:base base)
 
 define digit-writer(#:base base)
-  define digits find-digits(digitss #:base base)
-  define write-digit(d)
+  def digits = find-digits(digitss #:base base)
+  def write-digit(d) =
     write-byte(bytes-ref(digits d))
   write-digit
 
@@ -32,16 +34,17 @@ define write-spaces(n)
   for ([i in-range(n)]) write-char(#\space)
 
 define b-writer/w(#:width w #:base base)
-  define write-b b-writer(#:base base)
-  define write-b/w(b)
+  def write-b = b-writer(#:base base)
+  def write-b/w(b) =
     write-spaces{w - length(b)}
     write-b(b)
   write-b/w
 
 define b-writer(#:base base)
-  define write-digit digit-writer(#:base base)
-  define write-b(b)
-    for ([d in-list(reverse(b))]) write-digit(d)
+  def write-digit = digit-writer(#:base base)
+  def write-b(b) =
+    for ([d in-list(reverse(b))])
+      write-digit(d)
   write-b
 
 define writer/ln(writer)(x)
@@ -49,16 +52,16 @@ define writer/ln(writer)(x)
   newline()
 
 define b-writer/ln(#:base base)
-  writer/ln(b-writer(#:base base))
+  writer/ln b-writer(#:base base)
 
 define b-writer/w/ln(#:width w #:base base)
-  writer/ln(b-writer/w(#:width w #:base base))
+  writer/ln b-writer/w(#:width w #:base base)
 
-define display-base-n-fibs(f0 f1 #:up-to n #:base base)
+define display-base-n-fibs[f0 f1 #:up-to n #:base base]
   def write-b = b-writer/ln(#:base base)
   printf("base: ~v, digits: " base)
   write-b(base.range.reverse)
-  def rr-fibs = reversed-base-n-fibs(f0 f1 #:up-to n #:base base)
+  def rr-fibs = reversed-base-n-fibs[f0 f1 #:up-to n #:base base]
   def write-b/w = b-writer/w/ln(#:width rr-fibs.first.length #:base base)
   def n.len = n.number->string.string-length
   for ([r-fib (in-list reverse(rr-fibs))] [i in-naturals()])
@@ -67,17 +70,17 @@ define display-base-n-fibs(f0 f1 #:up-to n #:base base)
     printf("~a | " i.str)
     write-b/w(r-fib)
 
-define reversed-base-n-fibs(f0 f1 #:up-to up-to-n #:base base)
-  base-n-fibs-loop(list(f1 f0) 2 #:up-to up-to-n #:base base)
+define reversed-base-n-fibs[f0 f1 #:up-to up-to-n #:base base]
+  base-n-fibs-loop[list(f1 f0) 2 #:up-to up-to-n #:base base]
 
-define base-n-fibs(f0 f1 #:up-to up-to-n #:base base)
-  reverse(reversed-base-n-fibs(f0 f1 #:up-to up-to-n #:base base))
+define base-n-fibs[f0 f1 #:up-to up-to-n #:base base]
+  reverse reversed-base-n-fibs[f0 f1 #:up-to up-to-n #:base base]
 
 define fib(n #:base base)
   match n [0 '(0)] [1 '(1)]
-    [n first(reversed-base-n-fibs('(0) '(1) #:up-to n #:base base))]
+    n $ first reversed-base-n-fibs('(0) '(1) #:up-to n #:base base)
 
-define base-n-fibs-loop(accum n #:up-to up-to-n #:base base)
+define base-n-fibs-loop[accum n #:up-to up-to-n #:base base]
   my-cond
     if {up-to-n < n}
       accum
@@ -85,7 +88,7 @@ define base-n-fibs-loop(accum n #:up-to up-to-n #:base base)
       def f-1 = first(accum)
       def f-2 = second(accum)
       def f = b+[f-2 f-1 #:base base]
-      base-n-fibs-loop(cons(f accum) {n + 1} #:up-to up-to-n #:base base)
+      base-n-fibs-loop[(cons f accum) {n + 1} #:up-to up-to-n #:base base]
 
 define first+rest/0+null(lst)
   my-cond
@@ -99,8 +102,8 @@ define b+[b1 b2 #:base base]
     my-cond
       if {empty?(b1) and empty?(b2)}
         match carry
-          [0 reverse(rb)]
-          [n reverse(cons(n rb))]
+          0 $ reverse rb
+          n $ reverse cons(n rb)
       else
         define-values [b1.fst b1.rst] first+rest/0+null(b1)
         define-values [b2.fst b2.rst] first+rest/0+null(b2)
@@ -110,11 +113,13 @@ define b+[b1 b2 #:base base]
   loop[b1 b2 '() 0]
 
 define q/r[n #:base base]
-  define loop(q n)
+  define loop[q n]
     my-cond
-      if {n < base} values(q n)
-      else loop({q + 1} {n - base})
-  loop(0 n)
+      if {n < base}
+        values[q n]
+      else
+        loop[{q + 1} {n - base}]
+  loop[0 n]
 
 module+ test
   test-case "b+"
--- other-stuff.rkt
+++ other-stuff.rkt
@@ -11,7 +11,7 @@ module+ test
 
 define find-repeating-pattern(n #:base b)
   def f0 = make-list(n 0)
-  def f1 = cons(1 make-list({n - 1} 0))
+  def f1 = cons[1 make-list({n - 1} 0)]
   reverse(find-repeating-pattern-loop(f0 f1 list(f1 f0) n #:base b))
 
 define find-repeating-pattern-loop(f0 f1 rev-accum n #:base b)
@@ -19,22 +19,26 @@ define find-repeating-pattern-loop(f0 f1 rev-accum n #:base b)
     list-rest[(== f1) (== f0) {rst and cons(_ _)}]
       rst
     list-rest[fn fn-1 _]
-      def next = take[b+[fn-1 fn #:base b] n]
+      def next = $ take b+[fn-1 fn #:base b] n
       find-repeating-pattern-loop(f0 f1 cons(next rev-accum) n #:base b)
 
 define display-repeated-pattern(n #:base b)
   def write-b = b-writer/ln(#:base b)
   printf("base: ~v, digits: " b)
   write-b(b.range.reverse)
-  for-each[write-b find-repeating-pattern(n #:base b)]
+  for-each write-b find-repeating-pattern(n #:base b)
 
 define find-rep-pat-lengths(n)
-  for/list ([i in-range(2 add1(n))])
-    list(i length(find-repeating-pattern(1 #:base i)))
+  for/list ([i (in-range 2 {n + 1})])
+    list[i length(find-repeating-pattern(1 #:base i))]
 
 define plot-rep-pat-length-scatter-plot(n #:rep-pat-lengths [rep-pat-lengths find-rep-pat-lengths(n)])
-  plot[list(axes() tick-grid() points[rep-pat-lengths #:sym 'point #:color "red"])
-       #:x-min -5 #:x-max n #:y-min -5]
+  plot
+    list axes() tick-grid()
+      points[rep-pat-lengths #:sym 'point #:color "red"]
+    #:x-min -5
+    #:x-max n
+    #:y-min -5
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -66,7 +70,7 @@ module+ test
   display-repeated-pattern(4 #:base 2)
   define rep-pat-lengths
     find-rep-pat-lengths(10000)
-  for ([p in-list(rep-pat-lengths)] [_ in-range(21)])
+  for ([p (in-list rep-pat-lengths)] [_ (in-range 21)])
     match-define list(i len) p
     printf("length(find-repeating-pattern(1 #:base ~v)) = ~v\n" i len)
   argmax(second rep-pat-lengths)
